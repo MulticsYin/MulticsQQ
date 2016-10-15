d150bf74d1fa6417daa83639cf2c685694a3992c
MulticsYin <multics_luo@163.com>
2016-10-15 21:27:25 +0800

    update table.sql

--- table.sql
+++ table.sql
@@ -1,30 +1,29 @@
 create database chat;
-create table user_infor
-( 
-id int primary key,
-name varchar(20) not null,
-password varchar(10) not null,
-regtime datetime not null
+use chat;
+create table user_infor( 
+    id       int primary key,
+    name     varchar(20) not null,
+    password varchar(10) not null,
+    regtime  datetime not null
 );
 
-create table user_chat
-( 
-recid int auto_increment primary key,
-srcid int not null,
-destid int ,
-type int not null,
-content varchar(200) not null,
-centime datetime not null,
-strip varchar(15) not null,
-port int not null
+create table user_chat( 
+    recid   int auto_increment primary key,
+    srcid   int not null,
+    destid  int ,
+    type    int not null,
+    content varchar(200) not null,
+    centime datetime not null,
+    strip   varchar(15) not null,
+    port    int not null
 );
-create table user_log
-( 
-recid int auto_increment primary key,
-srcid int not null,
-type int not null,
-protime datetime not null,
-proendtime datetime,
-strip varchar(15) not null,
-port int not null
+
+create table user_log( 
+    recid   int auto_increment primary key,
+    srcid   int not null,
+    type    int not null,
+    protime datetime not null,
+    proendtime datetime,
+    strip   varchar(15) not null,
+    port    int not null
 );
