a7465fd58a2bc4dcaac705d10a2175c4a8fc2344
MulticsYin <multics_luo@163.com>
2016-12-14 20:10:52 +0800

    add some comment

--- server/compile.sh
+++ server/compile.sh
@@ -1,3 +1,3 @@
 #该文件告诉你怎么编译，实际情况下要视系统而定，在我的系统下我直接拷贝命令编译
-rm -f IMServer
-g++ -o IMServer  $(mysql_config --cflags) IMServer.cpp  $(mysql_config --libs) -lpthread
+rm -f MulticsQQ_Server
+g++ -o MulticsQQ_Server  $(mysql_config --cflags) MulticsQQ_Server.cpp  $(mysql_config --libs) -lpthread
